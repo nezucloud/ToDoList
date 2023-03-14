35a12474ae6d97bb985b3d723195bfbdc967ba4e
Triananda <nanaenezuko@github.com>
2023-03-14 09:32:23 +0700

    add some comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Get files assets
   get 'files/assets/images/:file_name', to: "assets#images"
 
+  # Dashboard
   get 'dashboard', to: "pages#dashboard"
   
   # Tasks
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
   patch 'task/update', to: "tasks#update"
   delete 'task/delete/:id', to: "tasks#delete"
 
+  # Users
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   devise_scope :user do
     get 'auth/show', to: 'users/sessions#show'
