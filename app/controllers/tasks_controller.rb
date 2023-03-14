1dc120b638119152a8cbc8b28b318d6ac8cefe17
Triananda <nanaenezuko@github.com>
2023-03-14 09:30:30 +0700

    Cleaning code for tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,24 @@
 class TasksController < ApplicationController
+    # show user tasks with parameters
     def show
-        sort_by = params[:sortby]
-        filter_by = params[:filterby]
-        hide_finished = params[:hide_finished]
+        # Params exists in the method
+        sort_by         = params[:sortby]
+        filter_by       = params[:filterby]
+        hide_finished   = params[:hide_finished]
 
         @user_tasks = current_user.task
 
+        # conditions of the tasks if they have finished
         if hide_finished == "true"
             @user_tasks = @user_tasks.where("status != 'finished'")
         end
 
+        # Conditions of the tasks if they have filtered by today
         if filter_by == "today"
             @user_tasks = @user_tasks.where("DATE(due_date) = DATE(NOW())")
         end
 
+        # Case in parameter sort by
         case sort_by
             when 'due_date' 
                 @user_tasks = @user_tasks.order(due_date: :asc)
@@ -23,39 +28,38 @@ class TasksController < ApplicationController
                 @user_tasks = @user_tasks.where("priority IS NOT NULL").order(priority: :asc)
             end
 
-
-        return render json: {
-            "data" => @user_tasks, 
-            "message" => "Get user tasks",
-            "status" => hide_finished
-        }, status: :ok
+        # Send response to the client
+        return render json: success_respond_object(@user_tasks, "Get user tasks"), status: :ok
     end
 
+    # craete the user tasks
     def create
         @task = current_user.task.create(task_params)
 
         if @task.save
             if task_params[:priority]
+                #Update the priority
                 update_priority(@task, current_user)
             end
 
-            return render json: {
-                "data" => [], 
-                "message" => "Task Successfuly Created",
-                "status" => 200
-            }, status: :ok
+            # Respond with success
+            return render json: success_respond_object([], "Task Successfuly Created"), status: :ok
         else
+            # Respond with error
             return render json: { errors: @task.errors, params: task_params.inspect }, status: :unprocessable_entity
         end
     end
 
+    # update task
     def update
         @task = Task.find(params[:id])
         task_priority = @task.priority
 
+        #Update the priority
         @task.update(task_params)
 
         if @task.priority
+            # Task priority updated to current priority if user checked the "Set as priority"
             User.connection.execute("
                 UPDATE tasks SET priority = priority + 1 
                 WHERE user_id = #{current_user.id} 
@@ -64,29 +68,26 @@ class TasksController < ApplicationController
             )
         end
 
-        return render json: {
-            "data" => [], 
-            "message" => "Task Successfuly Updated",
-            "status" => 200
-        }, status: :ok
+        # Respond with success
+        return render json: success_respond_object([], "Task successfully updated"), status: :ok
     end
 
+    # delete task
     def delete
         @task = Task.find(params[:id])
         @task.destroy
 
-        return render json: {
-            "data" => [],
-            "message" => "Task Deleted",
-            "status" => 200
-        }, status: :ok
+        # Respond with success
+        return render json: success_respond_object([], "Task Deleted"), status: :ok
     end
 
     private 
+        # permit access for the parmeters
         def task_params
             params.require(:task).permit(:title, :description, :due_date, :priority, :status)
         end
 
+        # Method to update a task priority per user
         def update_priority (task,current_user)
             User.connection.execute("
                 UPDATE tasks SET priority = priority + 1 
@@ -94,5 +95,14 @@ class TasksController < ApplicationController
                 AND priority IS NOT NULL
                 AND id != #{task.id}"
             )
+        end
+        
+        # function to generate success response object
+        def success_respond_object (data, message)
+            return {
+                "data"      => data,
+                "message"   => message,
+                "status"    => 200
+            }
         end
 end
