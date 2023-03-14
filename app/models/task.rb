9ce2a165dfd8c66fb297e7f5a76a41d6a82fc935
Triananda <nanaenezuko@github.com>
2023-03-14 09:31:02 +0700

    add comment

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  # Rules for status
   VALID_STATUSES = ["finished", "process"]
 
+  # payload validation
   validates :title, presence: true
   validates :due_date, presence: true
   validates :status, presence: true, inclusion: { in: VALID_STATUSES }
